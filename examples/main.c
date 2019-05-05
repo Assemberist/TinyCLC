ed8ae3267c77dd5a0534ae98520f16cc7ff584ae
Architector <olegsajaxov@yandex.ru>
2019-05-05 20:55:43 +0300

    renderer_ascii.h triangle rasterization

--- examples/main.c
+++ examples/main.c
@@ -1,24 +1,83 @@
 #include <stdio.h>
 #include <time.h>
+#include "renderer_ascii.h"
 
-#define LINEAR_DONT_USE_PREDEFINED_TYPES
-#include "linear.h"
 
-LINEAR(float)
+RdVertex3 vshader(const RdVertex2* v, RdVertexShader2* shader){
+    mat3f_ptr scale = shader->matrices._data[0];
+    mat3f_ptr rotation = shader->matrices._data[1];
+    mat3f_ptr position = shader->matrices._data[2];
 
-int main(){
-    vec3(float) x = {0, 1, 2};
-    vec3(float) y = {-1, 0, 2};
-    vec3(float) z = {-5, 3, 4};
+    mat3(float) global = mat3_mul_mat3(float)(scale, rotation);
+    global = mat3_mul_mat3(float)(&global, position);
 
-    vec3(float) f = lininter2(float)(x, y, z);
+    vec3(float) temp = mat3_mul_vec3(float)((vec3(float)){v->position.x, v->position.y, 1.0f}, &global);
 
-    for(float x = 0; x <= 2; x++){
-        for(float y = -1; y <= 2; y++){
-            float z = vec3_mul_vec3(float)((vec3(float)){x, y, 1}, f);
-            printf("%f\n", z);
-        }
+    return (RdVertex3){temp, v->color};
+}
+
+float fshader(const vec2(int)* p, RdFragmentShader* shader){
+    float color = shader->attributes._data[0];
+
+    return color;
+}
+
+
+int main(int argc, char* argv[]){
+    // setup window
+    RdScreen win = rdCreateScreen(atol(argv[1]), atol(argv[2]));
+    vec3(float) viewport = rdCreateViewportIdentity(&win);
+    win.viewport = &viewport;
+
+    // setup geometry
+    RdVertex2 verts[3] = {
+        (RdVertex2){0.0f, 1.0f, 0.25f},
+        (RdVertex2){-1.0f, -1.0f, 0.55f},
+        (RdVertex2){1.0f, -1.0f, 0.84f}
+    };
+    vec2(size_t) indexes[3] = {
+        (vec2(size_t)){0, 1},
+        (vec2(size_t)){1, 2},
+        (vec2(size_t)){2, 0}
+    };
+
+    // setup matrices
+    mat3(float) transpose = mat3_identity(float);
+    mat3(float) rotate = mat3_identity(float);
+    mat3(float) scale = mat3_identity(float);
+
+    std_small_vector(mat3f_ptr) pos = std_small_vector_default(mat3f_ptr);
+    pos.push_back(&pos, &scale);
+    pos.push_back(&pos, &rotate);
+    pos.push_back(&pos, &transpose);
+
+    // setup shaders
+    RdVertexShader2 vsh = {
+        vshader,
+        pos
+    };
+    RdFragmentShader fsh = {
+        fshader,
+        std_small_vector_default(float)
+    };
+    fsh.attributes.push_back(&fsh.attributes, 0.0f);
+
+    RdProgram2 prog = {&vsh, &fsh};
+
+    // render
+    rdScale2((vec2(float)){0.5f, 0.5f}, &scale);
+
+    for(float i = 0; i < 180; i += 0.05f){
+        rdRotate2(i, &rotate);
+
+        rdClear(&win);
+        rdTriangle2(verts, verts + 1, verts + 2, &prog, &win);
+        // rdLines2(verts, indexes, 3, &prog, &win);
+        rdDRender(&win);
     }
+    rdRender(&win);
 
+    // free
+    rdDestroyScreen(&win);
     return 0;
 }
--- renderer_ascii.h
+++ renderer_ascii.h
@@ -33,7 +33,7 @@ SMALL_VECTORS(float)
 else if(X > 1.0f) X = 1.0f;
 
 // #define RD_DEBUG
-#define RD_SHOW_GRID
+// #define RD_SHOW_GRID
 // #define RD_WHITE_BACK
 
 /////////////////////////////////////////////////////
@@ -125,6 +125,36 @@ char _rdGetColor(float color){
     #endif
 
     return _color;
+}
+
+vec2(int) _rdMinScreenPoint(const vec2(int) p0, const vec2(int) p1, bool by_y){
+    if(!by_y){
+        if(p0.x < p1.x) return p0;
+        return p1;
+    }else{
+        if(p0.y < p1.y) return p0;
+        return p1;
+    }
+}
+vec2(int) _rdMaxScreenPoint(const vec2(int) p0, const vec2(int) p1, bool by_y){
+    if(!by_y){
+        if(p0.x > p1.x) return p0;
+        return p1;
+    }else{
+        if(p0.y > p1.y) return p0;
+        return p1;
+    }
+}
+vec2(int) _rdMidScreenPoint(const vec2(int) p0, const vec2(int) p1, const vec2(int) p2, bool by_y){
+    if(!by_y){
+        if((p0.x < p1.x && p0.x > p2.x) || (p0.x < p2.x && p0.x > p1.x)) return p0;
+        else if((p1.x < p2.x && p1.x > p0.x) || (p1.x < p0.x && p1.x > p2.x)) return p1;
+        return p2;
+    }else{
+        if((p0.y < p1.y && p0.y > p2.y) || (p0.y < p2.y && p0.y > p1.y)) return p0;
+        else if((p1.y < p2.y && p1.y > p0.y) || (p1.y < p0.y && p1.y > p2.y)) return p1;
+        return p2;
+    }
 }
 
 void rdTranslate2(vec2(float) v, mat3(float)* m){
@@ -269,6 +299,73 @@ void rdDRender(const RdScreen* s){
 }
 
 // hidden
+void _rdLine(vec2(int) p0, vec2(int) p1, vec2(float) colors, RdFragmentShader* frag, RdScreen* s){
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
+    size_t max = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float step_x = (float)dx / max;
+    float step_y = (float)dy / max;
+
+    vec2(float) colors_x = {0, max};
+    vec2(float) colors_f = lininter(float)(colors_x, colors);
+
+    #pragma omp parallel for schedule(guided)
+    for(size_t i = 0; i <= max; i++){
+        vec2(int) p = {p0.x + i * step_x, p0.y + i * step_y};
+
+        float color = vec2_mul_vec2(float)((vec2(float)){i, 1}, colors_f);
+        frag->attributes._data[0] = color;
+        _rdSetScreenPoint(&p, frag->shader(&p, frag), s);
+    }
+}
+void _rdTriangle(vec2(int) p0, vec2(int) p1, vec2(int) p2, vec3(float) colors, RdFragmentShader* frag, RdScreen* s){
+    vec2(int) min = _rdMinScreenPoint(_rdMinScreenPoint(p0, p1, false), p2, false);
+    vec2(int) mid = _rdMidScreenPoint(p0, p1, p2, false);
+    vec2(int) max = _rdMaxScreenPoint(_rdMaxScreenPoint(p0, p1, false), p2, false);
+
+    int dx0 = mid.x - min.x;
+    int dy0 = mid.y - min.y;
+    float s0 = (float)dy0 / dx0;
+
+    int dx1 = max.x - min.x;
+    int dy1 = max.y - min.y;
+    float s1 = (float)dy1 / dx1;
+
+    int dx2 = max.x - mid.x;
+    int dy2 = max.y - mid.y;
+    float s2 = (float)dy2 / dx2;
+
+    vec3(float) colors_x = (vec3(float)){p0.x, p1.x, p2.x};
+    vec3(float) colors_y = (vec3(float)){p0.y, p1.y, p2.y};
+    vec3(float) colors_f = lininter2(float)(colors_x, colors_y, colors);
+
+    // first triangle
+    for(int x = min.x; x <= mid.x; x++){
+        int to = s0 > 0 ? s0 * (x - min.x) + min.y : s1 * (x - min.x) + min.y;
+        int from = s0 < 0 ? s0 * (x - min.x) + min.y : s1 * (x - min.x) + min.y;
+        
+        for(int y = from; y <= to; y++){
+            float color = vec3_mul_vec3(float)((vec3(float)){x, y, 1}, colors_f);
+            frag->attributes._data[0] = color;
+            vec2(int) p = {x, y};
+            _rdSetScreenPoint(&p, frag->shader(&p, frag), s);
+        }
+    }
+
+    // second triangle
+    for(int x = mid.x; x <= max.x; x++){
+        int to = s2 > 0 ? s1 * (x - min.x) + min.y : s2 * (x - mid.x) + mid.y;
+        int from = s2 < 0 ? s1 * (x - min.x) + min.y : s2 * (x - mid.x) + mid.y;
+        
+        for(int y = from; y <= to; y++){
+            float color = vec3_mul_vec3(float)((vec3(float)){x, y, 1}, colors_f);
+            frag->attributes._data[0] = color;
+            vec2(int) p = {x, y};
+            _rdSetScreenPoint(&p, frag->shader(&p, frag), s);
+        } 
+    }
+}
 
 // single
 void rdPoint2(const RdVertex2* v, RdProgram2* prog, const RdScreen* s){
@@ -278,9 +375,35 @@ void rdPoint2(const RdVertex2* v, RdProgram2* prog, const RdScreen* s){
     prog->fshader->attributes._data[0] = v->color;
     _rdSetScreenPoint(&p, prog->fshader->shader(&p, prog->fshader), s);
 }
+void rdLine2(const RdVertex2* v0, const RdVertex2* v1, RdProgram2* prog, RdScreen* s){
+    RdVertex3 new_v0 = prog->vshader->shader(v0, prog->vshader);
+    RdVertex3 new_v1 = prog->vshader->shader(v1, prog->vshader);
+
+    vec2(int) p0 = _rdGetScreenPoint(new_v0.position, s);
+    vec2(int) p1 = _rdGetScreenPoint(new_v1.position, s);
+   
+    _rdLine(p0, p1, (vec2(float)){new_v0.color, new_v1.color}, prog->fshader, s);
+}
+void rdTriangle2(const RdVertex2* v0, const RdVertex2* v1, const RdVertex2* v2, RdProgram2* prog, RdScreen* s){
+    RdVertex3 new_v0 = prog->vshader->shader(v0, prog->vshader);
+    RdVertex3 new_v1 = prog->vshader->shader(v1, prog->vshader);
+    RdVertex3 new_v2 = prog->vshader->shader(v2, prog->vshader);
+
+    vec2(int) p0 = _rdGetScreenPoint(new_v0.position, s);
+    vec2(int) p1 = _rdGetScreenPoint(new_v1.position, s);
+    vec2(int) p2 = _rdGetScreenPoint(new_v2.position, s);
+
+    _rdTriangle(p0, p1, p2, (vec3(float)){new_v0.color, new_v1.color, new_v2.color}, prog->fshader, s);
+}
 
 // multiple
 void rdPoints2(const RdVertex2* verts, size_t count, RdProgram2* prog, const RdScreen* s){
     // #pragma omp parallel for schedule(guided)
     for(size_t i = 0; i < count; i++) rdPoint2(verts + i, prog, s);
+}
+
+void rdLines2(const RdVertex2* verts, vec2(size_t)* indexes, size_t indexes_count, RdProgram2* prog, RdScreen* s){
+    // #pragma omp parallel for schedule(guided)
+    for(size_t i = 0; i < indexes_count; i++)
+        rdLine2(verts + indexes[i].x, verts + indexes[i].y, prog, s);
 }
