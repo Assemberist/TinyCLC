010e4b116d66914c9943e1cc664e907cd1ca1add
architector1324 <olegsajaxov@yandex.ru>
2019-05-30 20:58:06 +0300

    vm256 VMInstanceDefault

--- vm256/vm256.h
+++ vm256/vm256.h
@@ -46,6 +46,7 @@ typedef struct VMInstance{
     vm_r256 r0, r1, r2, r3;
     vm_uint256_t rsp256, rsp128, rsp64, rsp32, rsp16, rsp8;
     vm_uint256_t rip;
+    vm_bool halt;
 
     // stacks
     vm_uint256_t stack256[VM_STACK_SIZE / 32];
@@ -56,22 +57,37 @@ typedef struct VMInstance{
     vm_uint8_t stack8[VM_STACK_SIZE];
 } VMInstance;
 
+#define VMInstanceDefault (VMInstance){\
+    .rip = {\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00,\
+        0x00, 0x00, 0x00, 0x00\
+    },\
+    .halt = false\
+};
+
 
 // reference to register
-#define VM_R8(reg, vm) ((vm_r8*)&(vm).r0)[reg % 128]
-#define VM_R16(reg, vm) ((vm_r16*)&(vm).r0)[reg % 64]
-#define VM_R32(reg, vm) ((vm_r32*)&(vm).r0)[reg % 32]
-#define VM_R64(reg, vm) ((vm_r64*)&(vm).r0)[reg % 16]
-#define VM_R128(reg, vm) ((vm_r128*)&(vm).r0)[reg % 8]
+#define VM_R8(reg, vm) ((vm_r8*)&(vm).r0)[(reg) % 128]
+#define VM_R16(reg, vm) ((vm_r16*)&(vm).r0)[(reg) % 64]
+#define VM_R32(reg, vm) ((vm_r32*)&(vm).r0)[(reg) % 32]
+#define VM_R64(reg, vm) ((vm_r64*)&(vm).r0)[(reg) % 16]
+#define VM_R128(reg, vm) ((vm_r128*)&(vm).r0)[(reg) % 8]
+#define VM_R256(reg, vm) ((vm_r256*)&(vm).r0)[(reg) % 4]
 
 
 // Instruction
 typedef enum _VM_INSTRUCTION_TYPE {FREE, SINGLE, DOUBLE, TRIPLE} VM_INSTRUCTION_TYPE;
 typedef enum _VM_OPERAND_TYPE {REGISTER, NUMBER, ADDRESS} VM_OPERAND_TYPE;
 typedef enum _VM_OPERAND_SIZE{
-    UINT8 = 1, UINT16 = 2,
-    UINT32 = 4, UINT64 = 8,
-    UINT128 = 16, UINT256 = 32
+    UINT8_T = 1, UINT16_T = 2,
+    UINT32_T = 4, UINT64_T = 8,
+    UINT128_T = 16, UINT256_T = 32
 } VM_OPERAND_SIZE;
 
 
@@ -103,14 +119,42 @@ typedef struct VMInstruction{
     const void* op2;
 } VMInstruction;
 
+
 /////////////////////////////////////////////////////
 //       BASE INSTRUCTION SET IMPLEMENTATIONS
 /////////////////////////////////////////////////////
 
 void _vm_go_adr(vm_uint256_t* adr, VMInstance* vm){
+    // go adr
     vm->rip = *adr;
 }
+void _vm_go_r(vm_uint8_t* reg, VMInstance* vm){
+    // go r256
+    vm_uint8_t _reg = *reg;
+    if(_reg >= 248)
+        vm->rip = VM_UINT256_T(VM_R256(_reg - 248, *vm));
+    else vm->halt = true;
+}
 
+void _vm_snd_r_r(vm_uint8_t* reg0, vm_uint8_t* reg1, VMInstance* vm){
+    // snd from_r, to_r
+    vm_uint8_t _reg0 = *reg0;
+    vm_uint8_t _reg1 = *reg1;
+
+    if(_reg0 < 128){
+
+    }else if(_reg0 < 128 + 64){
+
+    }else if(_reg0 < 128 + 64 + 32){
+
+    }else if(_reg0 < 128 + 64 + 32 + 16){
+
+    }else if(_reg0 < 128 + 64 + 32 + 16 + 8){
+
+    }else if(_reg0 < 128 + 64 + 32 + 16 + 8 + 4){
+
+    } else vm->halt = true;
+}
 
 /////////////////////////////////////////////////////
 //       GLOBAL INSTRUCTION DESCRIPTORS TABLE
@@ -121,13 +165,29 @@ VMInstructionDescriptorsTable GIDT = {
         (VMInstructionDescriptor){
             .itype = SINGLE,
             .op0_type = ADDRESS,
-            .op0_size = UINT32,
+            .op0_size = UINT32_T,
             .icode = {0x00, 0x00, 0x00, 0x01},
             .alias = "go",
             .impl = _vm_go_adr
+        },
+        (VMInstructionDescriptor){
+            .itype = SINGLE,
+            .op0_type = REGISTER,
+            .op0_size = UINT8_T,
+            .icode = {0x00, 0x00, 0x00, 0x02},
+            .alias = "go",
+            .impl = _vm_go_r
+        },
+        (VMInstructionDescriptor){
+            .itype = DOUBLE,
+            .op0_type = REGISTER, .op1_type = REGISTER,
+            .op0_size = UINT8_T, .op1_size = UINT8_T,
+            .icode = {0x00, 0x00, 0x00, 0x03},
+            .alias = "snd",
+            .impl = _vm_snd_r_r
         }
     },
-    .size = 1
+    .size = 3
 };
 
 
@@ -155,24 +215,26 @@ const VMInstructionDescriptor* vmFindInstruction(const vm_uint32_t* icode, VMIns
 }
 
 void vmExecInstruction(VMInstruction* instr, VMInstance* vm, VMInstructionDescriptorsTable* extra){
-    const VMInstructionDescriptor* desc = vmFindInstruction(instr->icode, extra);
+    if(vm->halt == false){
+        const VMInstructionDescriptor* desc = vmFindInstruction(instr->icode, extra);
 
-    if(desc != NULL){
-        switch (desc->itype){
-        case FREE:
-            VM_FREE_IMPL(desc->impl)(vm);
-            break;
-        case SINGLE:
-            VM_SINGLE_IMPL(desc->impl)(instr->op0, vm);
-            break;
-        case DOUBLE:
-            VM_DOUBLE_IMPL(desc->impl)(instr->op0, instr->op1, vm);
-            break;
-        case TRIPLE:
-            VM_TRIPLE_IMPL(desc->impl)(instr->op0, instr->op1, instr->op2, vm);
-            break;
-        default:
-            break;
+        if(desc != NULL){
+            switch (desc->itype){
+            case FREE:
+                VM_FREE_IMPL(desc->impl)(vm);
+                break;
+            case SINGLE:
+                VM_SINGLE_IMPL(desc->impl)(instr->op0, vm);
+                break;
+            case DOUBLE:
+                VM_DOUBLE_IMPL(desc->impl)(instr->op0, instr->op1, vm);
+                break;
+            case TRIPLE:
+                VM_TRIPLE_IMPL(desc->impl)(instr->op0, instr->op1, instr->op2, vm);
+                break;
+            default:
+                break;
+            }
         }
-    }
+    }else ; // do some exception here
 }
